38798eb859cd1604264cb9c313d4bab1cf541fb7
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-07-17 18:03:09 +0100

    add components components

--- beast-world/src/components/HornedBeast/hornedbeast.js
+++ beast-world/src/components/HornedBeast/hornedbeast.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './HornedBeast.css';
 
-export default function HornedBeast() {
+export default function HornedBeast(props) {
   return (
-    <div>HornedBeast</div>
+    <div className='beast'>
+      <h2 className='beast-tittle'>{props.tittle}</h2>
+      <img className='beast-image' src={props.imageURL} alt={props.keyword}></img>
+      <p className='beast__description'>{props.description}</p>
+    </div>
   )
 }
--- beast-world/src/components/Main/Main.js
+++ beast-world/src/components/Main/Main.js
@@ -3,9 +3,12 @@ import HornedBeast from '../HornedBeast/hornedbeast'
 
 export default function Main() {
   return (
-    <div>
-        <HornedBeast />
-        <HornedBeast />
+    <main>
+    <div className='hornedBeast-main'>
+        <HornedBeast tittle={"Chort"} imgUrl={"https://en.wikipedia.org/wiki/File:Das_ist_mein_lieber_Sohn.jpg"} description={"Caricature of Napoleon with a Chort"} keyword={"Chort"} horns={2}/>
+        <HornedBeast tittle={"Leshy"} imgUrl={"https://www.amazon.co.uk/Leshy-gaurdian-forest-Piyush-https://m.media-amazon.com/images/I/61ocrAyQRuL._SL1024_.jpg-ebook/dp/B0BW66L711"} description={"Leshy in the Forest"} keyword={"Leshy"} horns={2}/>
+        <HornedBeast tittle={"Indrik"} imgUrl={"https://64.media.tumblr.com/0280edf123dca8ecfb10b9823564652b/tumblr_pu9kcqlqv91vw5i2lo1_500.jpg"} description={"Angry Indrik runnin on soldier"} keyword={"Indrik"} horns={1}/>
     </div>
+    </main>
   )
 }
