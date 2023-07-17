c95664d44809296fb9d57cc466c9538731914b4d
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-07-17 11:15:31 +0100

    secondary commit

---
+++ class-01-react/src/Row/Row.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Row() {
+    return (
+        <div className="row-div">
+            <div className='row-box'></div>
+            <div className='row-box'></div>
+            <div className='row-box'></div>
+        </div>
+    );
+}
